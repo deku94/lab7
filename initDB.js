4301abc63881f15186e7a20d3693d4abb8b7558f
Dennis <deku@ucsd.edu>
2016-02-11 21:34:01 +0000

    DB connect

--- initDB.js
+++ initDB.js
@@ -19,7 +19,7 @@ var models   = require('./models');
 var local_database_name = 'crescendo';
 var local_database_uri  = 'mongodb://localhost/' + local_database_name
 var database_uri = process.env.MONGOLAB_URI || local_database_uri
-console.log("URI: "+database_uri);
+console.log(database_uri);
 
 mongoose.connect(database_uri);
 
