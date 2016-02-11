92f9c2480caa6ae504c3de8921a6fd1430d5c55c
Dennis <deku@ucsd.edu>
2016-02-11 21:29:52 +0000

    DB connect

--- initDB.js
+++ initDB.js
@@ -16,23 +16,24 @@ var models   = require('./models');
 
 // Connect to the Mongo database, whether locally or on Heroku
 // MAKE SURE TO CHANGE THE NAME FROM 'lab7' TO ... IN OTHER PROJECTS
-var local_database_name = 'lab7';
+var local_database_name = 'crescendo';
 var local_database_uri  = 'mongodb://localhost/' + local_database_name
 var database_uri = process.env.MONGOLAB_URI || local_database_uri
+console.log("URI: "+database_uri);
+
 mongoose.connect(database_uri);
 
 
 // Do the initialization here
 
 // Step 1: load the JSON data
-var projects_json = require('./projects.json');
+var projects_json = require('./equipment.json');
 
 // Step 2: Remove all existing documents
-models.Project
+models.Equipment
   .find()
   .remove()
   .exec(onceClear); // callback to continue at
-
 // Step 3: load the data from the JSON file
 function onceClear(err) {
   if(err) console.log(err);
@@ -42,7 +43,40 @@ function onceClear(err) {
   var to_save_count = projects_json.length;
   for(var i=0; i<projects_json.length; i++) {
     var json = projects_json[i];
-    var proj = new models.Project(json);
+    var proj = new models.Equipment(json);
+
+    proj.save(function(err, proj) {
+      if(err) console.log(err);
+
+      to_save_count--;
+      console.log(to_save_count + ' left to save');
+      if(to_save_count <= 0) {
+        console.log('DONE');
+        // The script won't terminate until the 
+        // connection to the database is closed
+        //mongoose.connection.close()
+      }
+    });
+  }
+}
+// Step 1: load the JSON data
+var projects_json2 = require('./environment.json');
+
+// Step 2: Remove all existing documents
+models.Environment
+  .find()
+  .remove()
+  .exec(onceClear2); // callback to continue at
+// Step 3: load the data from the JSON file
+function onceClear2(err) {
+  if(err) console.log(err);
+
+  // loop over the projects, construct and save an object from each one
+  // Note that we don't care what order these saves are happening in...
+  var to_save_count = projects_json2.length;
+  for(var i=0; i<projects_json2.length; i++) {
+    var json = projects_json2[i];
+    var proj = new models.Environment(json);
 
     proj.save(function(err, proj) {
       if(err) console.log(err);
